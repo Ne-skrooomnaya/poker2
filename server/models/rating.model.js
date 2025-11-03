a95248c7433e83533c9205c5595483938777976c
ne_skrooomnaya <angelf04032005@gmail.com>
2025-11-03 16:27:37 +0300

    testfull25

--- server/models/rating.model.js
+++ server/models/rating.model.js
@@ -1,18 +1,13 @@
-// server/models/rating.model.js
 const mongoose = require('mongoose');
 
 const ratingSchema = new mongoose.Schema({
-  telegramId: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  score: {
-    type: Number,
-    default: 0,
-  },
-}, {
-  timestamps: true,
-});
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true, unique: true },
+    score: { type: Number, default: 0 },
+    username: { type: String, required: true },
+    firstName: { type: String },
+    telegramId: { type: String, required: true, unique: true },
+}, { timestamps: true });
 
-module.exports = mongoose.model('Rating', ratingSchema);
+const Rating = mongoose.model('Rating', ratingSchema);
+
+module.exports = Rating;
