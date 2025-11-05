2dfb5062ccc7d83c38c33fafd5e95d981c6601ce
ne_skrooomnaya <angelf04032005@gmail.com>
2025-11-06 00:20:20 +0300

    versiontgdeletefull1

---
+++ client/src/components/UserPage.js
@@ -0,0 +1,38 @@
+// client/src/components/AdminPage.js
+import React from 'react';
+import './UserPage.css';
+import { Link } from 'react-router-dom';
+import './HomePage.css';
+import logo from './images/logo.svg'; // Импортируйте ваш логотип
+
+const UserPage = ({ onLogout }) => {
+  return (
+    <>
+    {/* Логотип */}
+          <div className="logo-container">
+            <img src={logo} alt="Poker Logo" />
+          </div>
+   {/* Верхний блок */}
+
+           
+           <div className="bottom-section">
+             <Link to="/rating" className="t">
+               <button className="bottom-section-button">Рейтинг</button>
+             </Link>
+             <Link to="/rating" className="t">
+             <button className="bottom-section-button">Гонка месяца</button></Link>
+             <Link to="/rating" className="t">
+             <button className="bottom-section-button">Прошедшие игры</button></Link>
+             
+             
+           </div>
+
+             {/* Кнопка выхода */}
+      <button className="logout-button" onClick={onLogout}>
+        Выйти
+      </button>
+    </>
+  );
+};
+
+export default UserPage;
